0f80f7dc8f169bb9595f5479a1a219f262a18bfe
Rimon Moomey <rimon.moomey@gmail.com>
2022-10-31 16:37:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 + 1) - 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(10, 20, 15);
